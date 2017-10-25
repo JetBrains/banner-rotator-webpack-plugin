8a775432e298c1a67e469de99a2d777a3521f8e9
kisenka <qtuzov@gmail.com>
2017-10-25 20:11:36 +0300

    test: add some tests

---
+++ runtime/utils/__tests__/glob-to-matcher.test.js
@@ -0,0 +1,54 @@
+import globToMatcher from '../glob-to-matcher';
+
+const CASES = [
+  {
+    name: 'simple',
+    pattern: '/index.html',
+    results: {
+      '/': false,
+      '/index': false,
+      '/index.html': true
+    }
+  },
+  {
+    name: 'with asterisk',
+    pattern: '/foo/*',
+    results: {
+      '/': false,
+      '/index': false,
+      '/index.html/': false,
+      '/foo/bar': true,
+      '/fooz/foo/bar': false
+    }
+  },
+  {
+    name: 'with negation',
+    pattern: '!/foo/*',
+    results: {
+      '/': true,
+      '/index': true,
+      '/index.html/': true,
+      '/foo/bar': false,
+      '/fooz/foo/bar': true
+    }
+  }
+];
+
+describe('globToMatcher()', () => {
+  CASES.forEach(CASE => {
+    it(CASE.name, () => {
+      const { pattern, results } = CASE;
+      const matcher = globToMatcher(pattern);
+
+      Object.keys(results).forEach(input => {
+        const expected = CASE.results[input];
+        const result = matcher(input);
+
+        result.should.be.equal(
+          expected,
+          `\n\nPattern: ${pattern}\nInput: ${input}\nExpected: ${expected}\nActual: ${result}\n\n`
+        );
+      });
+    });
+  });
+});
--- runtime/utils/__tests__/matchers.test.js
+++ runtime/utils/__tests__/matchers.test.js
@@ -2,10 +2,31 @@
 import * as matchers from '../matchers';
 
 describe('matchers', () => {
-  describe('date', () => {
+  describe('date()', () => {
+    const date = matchers.date;
+
     it('should work', () => {
-      const res = matchers.date(new Date(2010), new Date(2012), new Date(2011));
+      const res = date(new Date(2010), new Date(2012), new Date(2011));
       res.should.be.true;
+    });
+  });
+
+  describe('location()', () => {
+    const location = matchers.location;
+
+    it('should properly match multiple patterns', () => {
+      location(['*', '!/index.html'], '/index.html').should.be.false;
+      location(['*', '!/foo/*'], '/foo').should.be.true;
+      location(['*', '!/foo/*'], '/foo/').should.be.false;
+    });
+  });
+
+  describe('country()', () => {
+    const country = matchers.country;
+
+    it('should work', () => {
+      country(['EN'], 'RU').should.be.false;
+      country(['EN', 'BG'], 'BG').should.be.true;
     });
   });
 });

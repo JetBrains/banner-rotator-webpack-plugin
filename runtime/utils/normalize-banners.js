aaa9a0ec4ac7487472d84cf08a909e8a1b55b84a
kisenka <qtuzov@gmail.com>
2017-11-15 21:36:01 +0300

    fix(runtime): return processed banners from normalizeBanners

--- runtime/utils/normalize-banners.js
+++ runtime/utils/normalize-banners.js
@@ -1,8 +1,14 @@
 import { parseDate } from './index';
 
+/**
+ * @param {Array<BannerConfig>} banners
+ * @return {Array<BannerConfig>}
+ */
 export default function normalizeBanners(banners) {
   banners.forEach(banner => {
     banner.startDate = banner.startDate ? parseDate(banner.startDate) : undefined;
     banner.endDate = banner.endDate ? parseDate(banner.endDate) : undefined;
   });
+
+  return banners;
 }
